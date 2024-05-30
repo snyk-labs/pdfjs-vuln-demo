f23acab799a9a2fa3abfe924fd4b80dfcfe3a52b
Brian Clark <clarkio@users.noreply.github.com>
2024-05-30 16:20:24 +0000

    add: vue working solution and more examples

--- src/components/VuePdfViewer.vue
+++ src/components/VuePdfViewer.vue
@@ -1,25 +1,15 @@
+
 <script setup lang="ts">
-import { ref } from 'vue';
-import { VuePdfEmbed } from 'vue-pdf-embed';
+import VuePdfEmbed from 'vue-pdf-embed';
 import 'vue-pdf-embed/dist/style/index.css'
 
-const pdfSrc = ref('/sample.pdf');
-
-const onFileChange = (event) => {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-  reader.onloadend = () => {
-    pdfSrc.value = URL.createObjectURL(file);
-  };
-  reader.readAsArrayBuffer(file);
-};
+const pdfSource = "/ex1.pdf";
 </script>
 
 <template>
   <div>
     <p>Vue Component</p>
-    <input type="file" @change="onFileChange" />
-    <VuePdfEmbed :source="sample.pdf" />
+    <VuePdfEmbed :source="pdfSource"/>
   </div>
 </template>
 
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'pdfjs-dist': 'pdfjs-dist/es5/build/pdf.js',
-      },
-    },
-  },
-};
